f4bd9f913b759c76f77fcc639b25018e17786763
yjjo <ojjy90@gmail.com>
2021-04-17 00:23:34 +0900

    modi struct

---
+++ vaccine_location/address_api/address.py
@@ -0,0 +1,58 @@
+
+
+def getLatLng(addr):
+    value = get_apikey(key_name="KAKAO_Authorization", json_filename="secret.json")
+    headers = {"Authorization": value}
+    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
+    result = json.loads(str(requests.get(url, headers=headers).text))
+    status_code = requests.get(url, headers=headers).status_code
+    if(status_code != 200):
+        print(f"ERROR: Unable to call rest api, http_status_coe: {status_code}")
+        return 0
+    # print(requests.get(url, headers=headers))
+    # print(result)
+
+    try:
+        match_first = result['documents'][0]['address']
+        lon = match_first['x']
+        lat = match_first['y']
+        # print(lon, lat)
+        # print(match_first)
+
+        return lon, lat
+    except IndexError: # match값이 없을때
+        print("IndexError발생, 해당 주소에 맞는 위도 경도 없음, 주소정보: ", addr)
+        raise IndexError
+    except TypeError: # match값이 2개이상일때
+        print("TypeError발생, 해당 주소에 맞는 위도 경도값 2개 이상, 주소정보: ", addr)
+        raise TypeError
+
+
+def getLatLng_list(addr_list):
+    key_name = "KAKAO_Authorization"
+    value = get_apikey(key_name, json_filename="secret.json")
+    headers={"Authorization": value}
+    lon_list=[]
+    lat_list=[]
+    for idx, addr in enumerate(addr_list):
+        url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
+        result = requests.get(url=url, headers=headers)
+        if(result.status_code != 200):
+            raise ValueError
+
+        # json으로 로드 하지 않으면 dict형식으로 불러올수 없다
+        addr_info = json.loads(result.text)
+        try:
+            addr_info_lon = addr_info["documents"][0]['address']['x']
+            addr_info_lat = addr_info["documents"][0]['address']['y']
+            lon_list.append(addr_info_lon)
+            lat_list.append(addr_info_lat)
+
+        except IndexError:  # match값이 없을때
+            print("IndexError발생, 해당 주소에 맞는 위도 경도 없음, idx: ",idx, "번째, 주소정보: ", addr)
+            raise IndexError
+        except TypeError:  # match값이 2개이상일때
+            print("TypeError발생, 해당 주소에 맞는 위도 경도값 2개 이상, idx: ",idx, "번째, 주소정보: ", addr)
+            raise TypeError
+
+    return lon_list, lat_list
--- vaccine_location/app.py
+++ vaccine_location/app.py
@@ -1,78 +1,25 @@
 from flask import Flask, render_template
 import pandas as pd
-from sqlalchemy import create_engine, update
-from folium import Map, Marker, Icon, Figure
-from folium.plugins import MarkerCluster
+from sqlalchemy import create_engine
+from folium import Map, Marker, Icon
 import folium
-import requests
-import json
-import os
-from pathlib import Path
 import pymysql
 
-# 데이터 갱신 -> check 주소 -> 테이블 업데이트 및 위도 경도 값 업데이트 -> csv파일 업데이트 // ->web csv읽어서 map 그려 or web db에서 읽어서 map그려
+from get_key import get_apikey
+from address_api.address import getLatLng, getLatLng_list
+
 app = Flask(__name__)
 # 애초에 주소 리스트를 생성하여 주소 찍을때 이 리스트를 넘겨 한번만 콜하여 지도에 찍는 방법이 있는지 강구.
-def get_apikey(key_name, json_filename):
-    # 해당 py파일의 속해 있는 폴더가 base_dir
-    BASE_DIR = Path(__file__).resolve().parent # == os.path.dirname(os.path.abspath(__file__))
-    # 해당 프로젝트 파일내 json파일이 있으므로 폴더패스와 파일이름을 합쳐 json_file의 절대경로값 얻는다
-    json_filepath = os.path.join(BASE_DIR, json_filename)
 
-    # json_file이 존재하지 않으면 error 발생
-    if(not os.path.isfile(json_filepath)):
-        print("JSON File Not Found")
-        raise FileNotFoundError
-
-    # json파일이 존재하면 json파일내의 모든 key, value값을 얻는다
-    with open(json_filepath) as f:
-        json_p = json.loads(f.read())
-        # print("json_p:  ", json_p)
-
-    try:
-        # key에 해당하는 value를 얻는다 ex. json_p["Authorization"]
-        value=json_p[key_name]
-        return value
-    except KeyError:
-        # 해당하는 key_name이 없는 경우이다
-        error_msg = "ERROR: Unvalid Key"
-        return error_msg
-
-def getVacStat():
-    value=get_apikey(key_name="VACCINE_Authorization", json_filename="secret.json")
-    header = {"Authorization":value}
-    url = "https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=90&perPage=7&serviceKey=pC39hvcQg6mW%2Bozlig8RxFoR40NkT%2FymzBYy8P9Sze7qQdWhzWISUXn2hIGSEB9d1XAOYy1IDb2U0VmkeVyRuQ%3D%3D"
-    result = json.loads(str(requests.get(url).text))
-    print(result)
+# def getVacStat():
+#     value=get_apikey(key_name="VACCINE_Authorization", json_filename="secret.json")
+#     header = {"Authorization":value}
+#     url = "https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=90&perPage=7&serviceKey=pC39hvcQg6mW%2Bozlig8RxFoR40NkT%2FymzBYy8P9Sze7qQdWhzWISUXn2hIGSEB9d1XAOYy1IDb2U0VmkeVyRuQ%3D%3D"
+#     result = json.loads(str(requests.get(url).text))
+#     print(result)
 # 잘못된 주소정보를 가져와도 에러가 발생하지 않도록 try except구문을 넣어 수정필요
 # if else구문으로 에러처리 할지 try except구문으로 에러 처리 할지 성능비교 필요
 
-def getLatLng(addr):
-    value = get_apikey(key_name="KAKAO_Authorization", json_filename="secret.json")
-    headers = {"Authorization": value}
-    url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
-    result = json.loads(str(requests.get(url, headers=headers).text))
-    status_code = requests.get(url, headers=headers).status_code
-    if(status_code != 200):
-        print(f"ERROR: Unable to call rest api, http_status_coe: {status_code}")
-        return 0
-    # print(requests.get(url, headers=headers))
-    # print(result)
-
-    try:
-        match_first = result['documents'][0]['address']
-        lon = match_first['x']
-        lat = match_first['y']
-        # print(lon, lat)
-        # print(match_first)
-
-        return lon, lat
-    except IndexError: # match값이 없을때
-        print("IndexError발생, 해당 주소에 맞는 위도 경도 없음, 주소정보: ", addr)
-        raise IndexError
-    except TypeError: # match값이 2개이상일때
-        print("TypeError발생, 해당 주소에 맞는 위도 경도값 2개 이상, 주소정보: ", addr)
-        raise TypeError
 
 def renew_data():
     """
@@ -82,8 +29,10 @@ def renew_data():
     df = pd.read_csv("csv/vac210414.csv")
     # check_error_addr(df)
     update_tables(df)
+    write_csv()
     print("PASS")
     return "PASS"
+
 # 초기 csv파일 에러 없는지 체크
 def check_error_addr(df):
     """
@@ -97,7 +46,7 @@ def check_error_addr(df):
     print("Pass")
     return addr_list, addr_lon_list, addr_lat_list
 
-# table생성 및 위도경도 업데이트
+# table생성 및 위도경도 테이블 업데이트
 def update_tables(df):
     sql = """update vaccine_center  set longitude = %s, latitude = %s where index_num = %s"""
     dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
@@ -122,34 +71,12 @@ def update_tables(df):
     for row in rows:
         print(row)
 
-def getLatLng_list(addr_list):
-    key_name = "KAKAO_Authorization"
-    value = get_apikey(key_name, json_filename="secret.json")
-    headers={"Authorization": value}
-    lon_list=[]
-    lat_list=[]
-    for idx, addr in enumerate(addr_list):
-        url = 'https://dapi.kakao.com/v2/local/search/address.json?query=' + addr
-        result = requests.get(url=url, headers=headers)
-        if(result.status_code != 200):
-            raise ValueError
+def write_csv():
+    conn = pymysql.connect(host='127.0.0.1', user='test', password='test', database='testdb')
+    sql_query = pd.read_sql_query('''select * from vaccine_center''', conn)
+    df = pd.DataFrame(sql_query)
+    df.to_csv("test.csv", index=False)
 
-        # json으로 로드 하지 않으면 dict형식으로 불러올수 없다
-        addr_info = json.loads(result.text)
-        try:
-            addr_info_lon = addr_info["documents"][0]['address']['x']
-            addr_info_lat = addr_info["documents"][0]['address']['y']
-            lon_list.append(addr_info_lon)
-            lat_list.append(addr_info_lat)
-
-        except IndexError:  # match값이 없을때
-            print("IndexError발생, 해당 주소에 맞는 위도 경도 없음, idx: ",idx, "번째, 주소정보: ", addr)
-            raise IndexError
-        except TypeError:  # match값이 2개이상일때
-            print("TypeError발생, 해당 주소에 맞는 위도 경도값 2개 이상, idx: ",idx, "번째, 주소정보: ", addr)
-            raise TypeError
-
-    return lon_list, lat_list
 
 @app.route('/test')
 def test_location():
@@ -203,14 +130,14 @@ def draw_map_multiple_function_call():
 @app.route('/')
 def draw_map_once_function_call():
     # db연결
-    dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
+    # dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
     # df = pd.read_csv("vac.csv")
     # df = pd.read_csv("vac210315.csv")
-    df = pd.read_csv("csv/vac210331.csv")
+    # df = pd.read_csv("csv/vac210331.csv")
     # dataframe내 데이터를 db에 넣는다 테이블이 없으면 생성하고 테이블과 데이터가 있으면 삭제하고 다시 생성
-    df.to_sql(name='vaccine_center', con=dbcon, if_exists='replace')
+    # df.to_sql(name='vaccine_center', con=dbcon, if_exists='replace')
     # # row갯수 만큼 for문을 돌아서 row들의 데이터를 각각 저장한다 iterrows()
-
+    df = pd.read_csv("test.csv")
 
     m = Map(location=[36.5053542, 127.7043419], zoom_start=8)
 
@@ -219,13 +146,15 @@ def draw_map_once_function_call():
         addr_list.append(df.loc[idx, "address"])
 
     # 여러번 호출하지 않고 함수는 1번만 호출하고 값들은 리스트 형태로 반환한다
-    lon_list, lat_list = getLatLng_list(addr_list)
+    # lon_list, lat_list = getLatLng_list(addr_list)
 
     for idx, addr in enumerate(addr_list):
         location_name=df.loc[idx, "location_name"]
+        latitude = df.loc[idx, "latitude"]
+        longitude = df.loc[idx, "longitude"]
         iframe = location_name + ":<br> " + addr
         popup = folium.Popup(iframe, min_width=200, max_width=200)
-        Marker(location=[lat_list[idx], lon_list[idx]], popup=popup, tooltip=location_name, icon=Icon(color='green', icon='flag')).add_to(m)
+        Marker(location=[latitude, longitude], popup=popup, tooltip=location_name, icon=Icon(color='green', icon='flag')).add_to(m)
     account_info=get_apikey("Account","secret.json")
     return render_template(template_name_or_list="index.html",
                            map=m._repr_html_(),
@@ -234,20 +163,23 @@ def draw_map_once_function_call():
 
 if __name__ == '__main__':
     # csv파일이 바뀌면 flask서버를 돌리기전 주소체크만 먼저 하고 주소의 오류가 없는지 먼저 검사한다.
-    renew_data()
+    # renew_data()
+    write_csv()
     # getVacStat()
     # #csv파일내 오류가 없으면 즉 모두 정확한 위도 경도 값 가지고 올수 있으면 flask서버 실행
-    # host_addr = '0.0.0.0'
-    # port_num = '5000'
-    # app.run(host=host_addr, port=port_num, debug=True)
+    host_addr = '0.0.0.0'
+    port_num = '5000'
+    app.run(host=host_addr, port=port_num, debug=True)
 
 
 
 # References
 # https://www.geeksforgeeks.org/different-ways-to-iterate-over-rows-in-pandas-dataframe/
 # https://www.geeksforgeeks.org/python-pandas-dataframe-loc/
-# Address http://yejinjo.com
 # https://gist.github.com/madan712/f27ac3b703a541abbcd63871a4a56636
+
+# Address http://yejinjo.com
+
 # csv파일내 주소 변경 사항
 # 10,지역,코로나19 대전광역시 유성구 예방접종센터,,유성종합스포츠센터,34128,대전광역시 유성구 유성대로 976 => 978
 # 63,지역,코로나19 전라남도 광양시 예방접종센터 ,,광양실내체육관,57728,전남 광양시 봉강면 매천 695-20 => 광양읍
