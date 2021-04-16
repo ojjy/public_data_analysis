afeffc365d539e24d94ebbe9f43523aaa407160b
yjjo <ojjy90@gmail.com>
2021-04-16 16:24:16 +0900

    add

--- vaccine_location/app.py
+++ vaccine_location/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 from folium import Map, Marker, Icon, Figure
 from folium.plugins import MarkerCluster
 import folium
@@ -8,7 +8,9 @@ import requests
 import json
 import os
 from pathlib import Path
+import pymysql
 
+# 데이터 갱신 -> check 주소 -> 테이블 업데이트 및 위도 경도 값 업데이트 -> csv파일 업데이트 // ->web csv읽어서 map 그려 or web db에서 읽어서 map그려
 app = Flask(__name__)
 # 애초에 주소 리스트를 생성하여 주소 찍을때 이 리스트를 넘겨 한번만 콜하여 지도에 찍는 방법이 있는지 강구.
 def get_apikey(key_name, json_filename):
@@ -35,7 +37,6 @@ def get_apikey(key_name, json_filename):
         # 해당하는 key_name이 없는 경우이다
         error_msg = "ERROR: Unvalid Key"
         return error_msg
-
 
 def getVacStat():
     value=get_apikey(key_name="VACCINE_Authorization", json_filename="secret.json")
@@ -73,40 +74,58 @@ def getLatLng(addr):
         print("TypeError발생, 해당 주소에 맞는 위도 경도값 2개 이상, 주소정보: ", addr)
         raise TypeError
 
-
 def renew_data():
     """
     csv 파일이 업데이트 되면 호출하는 함수로 csv파일 체크 하고 table에 csv데이터를 넣는다.
     :return:
     """
-    df = pd.read_csv("csv/vac210407.csv")
-    check_error_addr(df)
+    df = pd.read_csv("csv/vac210414.csv")
+    # check_error_addr(df)
     update_tables(df)
     print("PASS")
     return "PASS"
-
+# 초기 csv파일 에러 없는지 체크
 def check_error_addr(df):
     """
     주소에러 체크 csv 파일 내 올바른 위도경도 값을 찾을수 있도록 지도로 찍기전 getLanLat함수를 호출하여 오류 검사를 한다.
     :return:
     """
-
     addr_list=[]
     for idx in range(len(df)):
-        addr_list.append(df.loc[idx, "주소"])
+        addr_list.append(df.loc[idx, "address"])
     addr_lon_list, addr_lat_list = getLatLng_list(addr_list)
     print("Pass")
-    return "Pass"
+    return addr_list, addr_lon_list, addr_lat_list
 
+# table생성 및 위도경도 업데이트
 def update_tables(df):
+    sql = """update vaccine_center  set longitude = %s, latitude = %s where index_num = %s"""
     dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
+    conn = pymysql.connect(host='127.0.0.1', user='test', password='test', database='testdb')
+    cur = conn.cursor()
     # dataframe내 데이터를 db에 넣는다 테이블이 없으면 생성하고 테이블과 데이터가 있으면 삭제하고 다시 생성
-    df.to_sql(name='vaccine_loc', con=dbcon, if_exists='replace')
+    df.to_sql(name='vaccine_center', con=dbcon, if_exists='replace')
+    addr_list, addr_lon_list, addr_lat_list = check_error_addr(df)
+
+    for idx, addr in enumerate(addr_list):
+        # dbcon.execute(sql)
+        # stmt = (
+        #     update("vaccine_center").
+        #     where("vaccine_center".index_num == idx).
+        #     value(longitude=addr_lon_list[idx], latitude=addr_lat_list[idx])
+        # )
+        cur.execute(sql, (addr_lon_list[idx], addr_lat_list[idx], idx+1))
+        conn.commit()
+
+    cur.execute("select * from vaccine_center")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
 def getLatLng_list(addr_list):
-    key_name = "Authorization"
+    key_name = "KAKAO_Authorization"
     value = get_apikey(key_name, json_filename="secret.json")
-    headers={key_name: value}
+    headers={"Authorization": value}
     lon_list=[]
     lat_list=[]
     for idx, addr in enumerate(addr_list):
@@ -157,12 +176,12 @@ def base():
 @app.route('/map')
 def draw_map_multiple_function_call():
     # db연결
-    # dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
+    dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
     # df = pd.read_csv("vac.csv")
     # df = pd.read_csv("vac210315.csv")
     df = pd.read_csv("csv/vac210407.csv")
     # dataframe내 데이터를 db에 넣는다 테이블이 없으면 생성하고 테이블과 데이터가 있으면 삭제하고 다시 생성
-    # df.to_sql(name='vaccine_loc', con=dbcon, if_exists='replace')
+    df.to_sql(name='vaccine_center', con=dbcon, if_exists='replace')
     # # row갯수 만큼 for문을 돌아서 row들의 데이터를 각각 저장한다 iterrows()
 
     m = Map(location=[36.5053542, 127.7043419], zoom_start=8)
@@ -170,8 +189,8 @@ def draw_map_multiple_function_call():
     # print(df)
     for idx in range(len(df)):
         # print(df.loc[idx, "시설명"], df.loc[idx, "주소"])
-        location_name = df.loc[idx, "시설명"]
-        addr = df.loc[idx, "주소"]
+        location_name = df.loc[idx, "location_name"]
+        addr = df.loc[idx, "address"]
         addr_lon, addr_lat = getLatLng(addr)
         iframe = location_name+ ":<br> "+ addr
         popup = folium.Popup(iframe, min_width=200, max_width=200)
@@ -184,12 +203,12 @@ def draw_map_multiple_function_call():
 @app.route('/')
 def draw_map_once_function_call():
     # db연결
-    # dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
+    dbcon = create_engine("mysql+pymysql://test:test@127.0.0.1/testdb")
     # df = pd.read_csv("vac.csv")
     # df = pd.read_csv("vac210315.csv")
     df = pd.read_csv("csv/vac210331.csv")
     # dataframe내 데이터를 db에 넣는다 테이블이 없으면 생성하고 테이블과 데이터가 있으면 삭제하고 다시 생성
-    # df.to_sql(name='vaccine_loc', con=dbcon, if_exists='replace')
+    df.to_sql(name='vaccine_center', con=dbcon, if_exists='replace')
     # # row갯수 만큼 for문을 돌아서 row들의 데이터를 각각 저장한다 iterrows()
 
 
@@ -197,13 +216,13 @@ def draw_map_once_function_call():
 
     addr_list=[]
     for idx in range(len(df)):
-        addr_list.append(df.loc[idx, "주소"])
+        addr_list.append(df.loc[idx, "address"])
 
     # 여러번 호출하지 않고 함수는 1번만 호출하고 값들은 리스트 형태로 반환한다
     lon_list, lat_list = getLatLng_list(addr_list)
 
     for idx, addr in enumerate(addr_list):
-        location_name=df.loc[idx, "시설명"]
+        location_name=df.loc[idx, "location_name"]
         iframe = location_name + ":<br> " + addr
         popup = folium.Popup(iframe, min_width=200, max_width=200)
         Marker(location=[lat_list[idx], lon_list[idx]], popup=popup, tooltip=location_name, icon=Icon(color='green', icon='flag')).add_to(m)
@@ -215,8 +234,8 @@ def draw_map_once_function_call():
 
 if __name__ == '__main__':
     # csv파일이 바뀌면 flask서버를 돌리기전 주소체크만 먼저 하고 주소의 오류가 없는지 먼저 검사한다.
-    # renew_data()
-    getVacStat()
+    renew_data()
+    # getVacStat()
     # #csv파일내 오류가 없으면 즉 모두 정확한 위도 경도 값 가지고 올수 있으면 flask서버 실행
     # host_addr = '0.0.0.0'
     # port_num = '5000'
@@ -228,7 +247,7 @@ if __name__ == '__main__':
 # https://www.geeksforgeeks.org/different-ways-to-iterate-over-rows-in-pandas-dataframe/
 # https://www.geeksforgeeks.org/python-pandas-dataframe-loc/
 # Address http://yejinjo.com
-
+# https://gist.github.com/madan712/f27ac3b703a541abbcd63871a4a56636
 # csv파일내 주소 변경 사항
 # 10,지역,코로나19 대전광역시 유성구 예방접종센터,,유성종합스포츠센터,34128,대전광역시 유성구 유성대로 976 => 978
 # 63,지역,코로나19 전라남도 광양시 예방접종센터 ,,광양실내체육관,57728,전남 광양시 봉강면 매천 695-20 => 광양읍
