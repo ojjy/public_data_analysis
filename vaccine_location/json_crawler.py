156a12d8c7919b893d96885ea280d32e310152af
yjjo <ojjy90@gmail.com>
2021-05-24 21:04:02 +0900

    add

--- vaccine_location/json_crawler.py
+++ vaccine_location/json_crawler.py
@@ -4,17 +4,20 @@ import json
 import math
 
 def vaccine_location():
-    url = f"https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=263&returnType=json&serviceKey={get_apikey('serviceKey', 'secret.json')}"
+
+    url = f"https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page=1&perPage=10&serviceKey={get_apikey('serviceKey', 'secret.json')}"
     json_data = json.loads(requests.get(url=url).text)
-    print(json_data)
+    # print(json_data)
     maxpageno = math.ceil(json_data["totalCount"]/json_data["perPage"])
-    print(maxpageno)
+    # print(maxpageno)
     for pageno in range(1, maxpageno+1):
-        url = f"https://api.odcloud.kr/api/15077586/v1/centers?page={pageno}&perPage={json_data['perPage']}&serviceKey={get_apikey('serviceKey', 'secret.json')}"
+        url = f"https://api.odcloud.kr/api/15077756/v1/vaccine-stat?page={pageno}&perPage={json_data['perPage']}&serviceKey={get_apikey('serviceKey', 'secret.json')}"
         json_data = json.loads(requests.get(url).text)
         items = json_data["data"]
-        for idx, item in enumerate(items):
-            print(idx, item)
+        print(items[0])
+        # for idx, item in enumerate(items):
+        #     for key_value in item.items():
+        #         print(f"key_value: {key_value}")
 
 if __name__ == "__main__":
     vaccine_location()
